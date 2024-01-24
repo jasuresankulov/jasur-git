f87089b61cd16af27ba2ecced80e6a8ec880c34a
esankulov <jasuresankulov07@gmail.com>
2024-01-24 18:38:42 +0500

    Carousel

--- install/src/components/About/TravelBlog.jsx
+++ install/src/components/About/TravelBlog.jsx
@@ -1,17 +1,41 @@
 import { Link } from "react-router-dom";
 import './style.scss'
-
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 function TravelBlog() {
+
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 1024 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 800 },
+            items: 4
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 5
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 10000 },
+            items: 4
+        }
+    };
+
+
+
     return (
-        <>
-            <div className="travel-blog-wrapper">
-                <div className="travel-text">
-                    <h2>Популярные экскурсии</h2>
-                </div>
-                <div className="link"> <Link to={'/'}>Смотреть все</Link></div>
+        <div className="travel-blog-wrapper">
+            <div className="travel-text">
+                <h2>Популярные экскурсии</h2>
+            </div>
+            <div className="link"> <Link to={'/'}>Смотреть все</Link></div>
 
+            <div className="carousel-wrapper">
 
-                <div className="carousel-images">
+                <Carousel responsive={responsive}>
 
                     <div className="first">
 
@@ -31,6 +55,7 @@ function TravelBlog() {
 
                         </div>
                     </div>
+
                     <div className="second">
                         <div className="carousel-text">
                             <span>
@@ -48,6 +73,7 @@ function TravelBlog() {
                             <button className="info-btn">Подробнее</button>
                         </div>
                     </div>
+
                     <div className="third">
                         <div className="carousel-text">
                             <span>
@@ -65,6 +91,7 @@ function TravelBlog() {
                             <button className="info-btn">Подробнее</button>
                         </div>
                     </div>
+
                     <div className="fourth">
                         <div className="carousel-text">
                             <span>
@@ -78,17 +105,89 @@ function TravelBlog() {
                                 Экскурсия начнется с подъёма на гору Большой Ахун - это  высшая точка (665 метров) на побережьи Сочи. На вершине горы Большой Ахун есть смотровая башня, построенная в 1935-36 годах по проекту архитектора С. И. Воробьева...
                             </span>
                             <br />
-                             <br />
+                            <br />
                             <button className="info-btn">Подробнее</button>
                         </div>
 
 
                     </div>
 
-                </div>
-            </div>
-        </>
+                    <div className="first">
 
+                        <div className="carousel-text">
+                            <span>
+                                Автобусный тур
+                                <br />
+                                <span className="yello-text">Обзорная по Сочи (из Адлера)</span>
+                                6 часов
+                                618 ₽
+                                Приглашаем вас на экскурсию
+                                "Обзорная по Большому Сочи":
+                                Экскурсия начнется с подъёма на гору Большой Ахун - это  высшая точка (665 метров) на побережьи Сочи. На вершине горы Большой Ахун есть смотровая башня, построенная в 1935-36 годах по проекту архитектора С. И. Воробьева...
+                            </span>
+                            <br />
+                            <button className="info-btn">Подробнее</button>
+
+                        </div>
+                    </div>
+
+                    <div className="second">
+                        <div className="carousel-text">
+                            <span>
+                                Автобусный тур
+                                <br />
+                                <span className="yello-text">Обзорная по Сочи (из Адлера)</span>
+                                6 часов
+                                618 ₽
+                                Приглашаем вас на экскурсию
+                                "Обзорная по Большому Сочи":
+                                Экскурсия начнется с подъёма на гору Большой Ахун - это  высшая точка (665 метров) на побережьи Сочи. На вершине горы Большой Ахун есть смотровая башня, построенная в 1935-36 годах по проекту архитектора С. И. Воробьева...
+                            </span>
+                            <br />
+                            <br />
+                            <button className="info-btn">Подробнее</button>
+                        </div>
+                    </div>
+
+                    <div className="third">
+                        <div className="carousel-text">
+                            <span>
+                                Автобусный тур
+                                <br />
+                                <span className="yello-text">Обзорная по Сочи (из Адлера)</span>
+                                6 часов
+                                618 ₽
+                                Приглашаем вас на экскурсию
+                                "Обзорная по Большому Сочи":
+                                Экскурсия начнется с подъёма на гору Большой Ахун - это  высшая точка (665 метров) на побережьи Сочи. На вершине горы Большой Ахун есть смотровая башня, построенная в 1935-36 годах по проекту архитектора С. И. Воробьева...
+                            </span>
+                            <br />
+                            <br />
+                            <button className="info-btn">Подробнее</button>
+                        </div>
+                    </div>
+
+                    <div className="fourth">
+                        <div className="carousel-text">
+                            <span>
+                                Автобусный тур
+                                <br />
+                                <span className="yello-text">Обзорная по Сочи (из Адлера)</span>
+                                6 часов
+                                618 ₽
+                                Приглашаем вас на экскурсию
+                                "Обзорная по Большому Сочи":
+                                Экскурсия начнется с подъёма на гору Большой Ахун - это  высшая точка (665 метров) на побережьи Сочи. На вершине горы Большой Ахун есть смотровая башня, построенная в 1935-36 годах по проекту архитектора С. И. Воробьева...
+                            </span>
+                            <br />
+                            <br />
+                            <button className="info-btn">Подробнее</button>
+                        </div>
+                    </div>
+
+                </Carousel>;
+            </div>
+        </div>
     );
 }
 
--- install/src/components/About/index.jsx
+++ install/src/components/About/index.jsx
@@ -2,15 +2,23 @@ import "./style.scss"
 import aboutImg from "../../assets/images/aboutBackImg.png";
 import TravelBlog from "./TravelBlog";
 import Values from "./Values";
-
-
+ 
 function About() {
-        return (
+         return (
 
                 <div className="about-wrapper">
+
+                 
+
+
+
+
+
+
+
+
                         <div className="about-back-img">
-                                
-                        <div className="about-desc">
+                         <div className="about-desc">
                                 <h2>About Us</h2>
                                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt, quia voluptatibus eos blanditiis ex eum est. Modi minima dolore nostrum neque officiis odio saepe, vel tenetur autem perspiciatis. Temporibus, totam!</p>
                         </div>
